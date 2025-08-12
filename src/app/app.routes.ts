adb155970585f484298ca130203ea9112c768e17
RishitaSprintPark <rishitak@sprintpark.com>
2025-08-12 20:03:17 +0530

    insights

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
 import { HomeComponent } from './home/home.component';
-
-export const routes: Routes = [  { path: '', component: HomeComponent }, { path: '**', redirectTo: '' },];
+import { InsightsComponent } from './insights/insights.component';
+ 
+export const routes: Routes = [
+    {
+        path:'',component:HomeComponent
+    },
+    {
+        path:'insights',component:InsightsComponent
+    }
+];
+ 
--- src/app/insights/insights.component.ts
+++ src/app/insights/insights.component.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-insights',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './insights.component.html',
   styleUrl: './insights.component.scss'
 })
 export class InsightsComponent {
+ insights = [
+    {
+      image: '/assets/insights/1.jpg',
+      title: 'Achieving Marketing Excellence: Boost Your Campaigns Using Salesforce Marketing Cloud & Salesforce Data Cloud'
+    },
+    {
+      image: '/assets/insights/2.jpg',
+      title: 'Automate your business process through Salesforce Flow Builder'
+    },
+    {
+      image: '/assets/insights/3.jpg',
+      title: 'Demo: How the Salesforce Health Cloud changing Healthcare Industry'
+    },
+    {
+      image: '/assets/insights/4.jpg',
+      title: 'Exploring the Potential of Salesforce Analytics Cloud & Compliance Use-cases in Business Intelligence'
+    },
+    {
+      image: '/assets/insights/5.jpg',
+      title: 'Empowering smart Business Reporting via potential of Salesforce CRM Analytics'
+    },
+    {
+      image: '/assets/insights/6.jpg',
+      title: 'Exploring the trending benefits of Choosing Salesforce Consultations'
+    }
+  ];
 
 }
