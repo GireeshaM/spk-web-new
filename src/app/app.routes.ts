a72ae1f97fa3f6d9990266f65eb2f7c569003a4c
Gireesha <gireesharani.marreddy@sprintmap.in>
2025-08-13 17:10:11 +0530

    home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
 import { TeleCommunicationsComponent } from './webmodules/industries/tele-communications/tele-communications.component';
 
 export const routes: Routes = [
@@ -13,7 +12,3 @@ export const routes: Routes = [
     component: TeleCommunicationsComponent,
   },
 ];
-=======
-
-export const routes: Routes = [  { path: '', component: HomeComponent }, { path: '**', redirectTo: '' },];
->>>>>>> 1aea208467cfcb774cac93a43519200e95afd9eb
